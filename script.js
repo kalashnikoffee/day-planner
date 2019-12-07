b6e25e97a805420ffe04110f61722abaeb5bfdf9
Lucas Coffee <lsterlingcoffee@gmail.com>
2019-12-07 01:47:14 -0800

    include basic pseudocod to lay out project requirements/steps

---
+++ script.js
@@ -0,0 +1,5 @@
+//Something that will update the style in real time to highlight the appropriate row each hour. 
+//Take input on each row that will will either print or be visible to console.log
+//Save button on each Row to add to local storage and reapply on a refresh
+//display date and time at top of page
+//each row has 3 potential colors = past, present, future
