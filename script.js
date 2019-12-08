47613b124be2d93209ea666d3728a1f61a8a188c
Lucas Coffee <lsterlingcoffee@gmail.com>
2019-12-07 16:35:44 -0800

    clock made from javascript connected to body tag and topclock div

--- script.js
+++ script.js
@@ -3,3 +3,25 @@
 //Save button on each Row to add to local storage and reapply on a refresh
 //display date and time at top of page
 //each row has 3 potential colors = past, present, future
+
+//CLOCK----------------------------------
+
+function updateClock ( ) {
+    var currentTime = new Date ( );
+
+var currentHours = currentTime.getHours ( );
+var currentMinutes = currentTime.getMinutes ( );
+var currentSeconds = currentTime.getSeconds ( );
+
+currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+
+var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+
+document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+};
+
